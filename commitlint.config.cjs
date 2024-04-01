6a694afe00c9a49ec541a1ff1106bccebd56b37e
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2024-04-01 15:01:52 -0500

    fix: accept all conventional commit types

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,11 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(\w*)(?:\((.)\))?!?: (.)$/,
-      headerCorrespondence: ["type", "scope", "subject"],
-      noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
-    },
-  },
 };
